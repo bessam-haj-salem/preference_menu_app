c8334f24793c3476ff68f4085eb8633e9b38b2d7
bessam-haj-salem <bessamhajsalem@gmail.com>
2020-12-06 11:47:57 +0100

    open apps  done, now working on saving the apps

--- app.py
+++ app.py
@@ -2,8 +2,44 @@ import tkinter as tk  # GUI create
 # filedialog: picks the app, Text: display text
 from tkinter import filedialog, Text
 import os  # run the app
+import subprocess
+import sys
+
 
 root = tk.Tk()
+# create a list and append all the files created in it
+apps = []
+
+# function for the button
+
+# initialdir: working place where we want navigate from
+
+
+def addApp():
+    # widget all the things attached to the frame (labels)
+    for widget in frame.winfo_children():
+        # destroy everything and attach the updated version of it
+        widget.destroy()
+
+    filename = filedialog.askopenfilename(initialdir="/", title="Select File",
+                                          filetypes=(("executables", "*.pdf"), ("all files", "*.*")))
+    apps.append(filename)
+    # give us the path  of the file
+    print(filename)
+    # looping over the apps
+    for app in apps:
+        label = tk.Label(frame, text=app, bg="gray")
+        label.pack()
+
+# run the apps
+
+
+def runApps():
+    for app in apps:
+        opener = "open" if sys.platform == "darwin" else "xdg-open"
+        subprocess.call([opener, app])
+
+
 #   some CSS on the GUI
 canvas = tk.Canvas(root, height=700, width=700, bg="#263D42")
 # attach the canva
@@ -15,12 +51,18 @@ frame.place(relwidth=0.8, relheight=0.8, relx=0.1, rely=0.1)
 
 # add buttons : fg: color of the text,
 openFile = tk.Button(root, text="Open File", padx=10,
-                     pady=5, fg="white", bg="#263D42")
+                     pady=5, fg="white", bg="#263D42", command=addApp)
 # attach the button
 openFile.pack()
 # NB: the root is the squeleton (in grey), the frame is the div inside
 # add another button
-openFile = tk.Button(root, text="Open File", padx=10,
-                     pady=5, fg="white", bg="#263D42")
+runApps = tk.Button(root, text="Run Apps", padx=10,
+                    pady=5, fg="white", bg="#263D42", command=runApps)
+runApps.pack()
 # run the GUI interface
-# root.mainloop()
+root.mainloop()
+
+#save tht apps
+with open('save.txt', 'w') as f:
+    fro app
+
